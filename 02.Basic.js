08cb559e29387c4ed261ca240db8fe271af28f8e
Hyunsoo Lee <gustn6839@naver.com>
2020-09-18 17:00:05 +0900

    first commit

---
+++ 02.Basic.js
@@ -0,0 +1,56 @@
+//원 둘레와 면적
+let pi = 3.14159265;
+let radius = 5;
+
+//console.log(`반지름이 ${radius}인 원의 둘레는 ${2*pi*radius}입니다.`);
+//console.log(`반지름이 ${radius}인 원의 면적은 ${pi*radius*radius}입니다.`);
+//console.log(`둘레: ${2*pi*radius}`);
+//console.log(`넓이: ${pi*radius*radius}`);
+
+//문자열과 관련된 복합대입연산자
+/* let output = 'hello';
+output += ' world';
+output += '!';
+output += '숫자도 가능합니다.';
+output += '\n1234567890';
+output += '\n237745';
+console.log(output); */
+
+// 6가지 자료형
+/* console.log(typeof('String'));           //String
+console.log(typeof(273));                //Number
+console.log(typeof(true));               //bool
+console.log(typeof(function(){}));       //function
+console.log(typeof({a:2}));                 //object
+console.log(typeof(alpha));              //undefined */
+
+// 자료형 변환
+/* console.log(Number('273'));
+console.log(Number('true'));
+console.log(Number('52.7'));
+
+console.log(parseInt('273'));
+console.log(parseInt('true'));
+console.log(parseInt('52.7'));
+
+console.log(parseFloat('273'));
+console.log(parseFloat('true'));
+console.log(parseFloat('52.7')); */
+
+//숫자의 문자열 자동 변환
+/* console.log(52 + 273);
+console.log("52" + 273);
+console.log(52 + "273");
+console.log("52" + "273"); */
+
+//일치연산자
+/* console.log(`52 == "52" : ${52 == "52"}`);
+console.log(`52 === "52" : ${52 === "52"}`);
+console.log();
+console.log(`0 == "" : ${ 0 == ""}`);
+console.log(`0 === "" : ${0 === ""}`); */
+
+// 상수
+const constant = '상수';
+//constant = 'Constant';
+console.log(constant);
